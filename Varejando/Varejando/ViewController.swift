676e7a046274c2728e65337887f89f68fa652c65
Ruan Gustavo <ruan@mypper.com>
2020-02-08 23:33:44 -0300

    First Commit - Crete Project

---
+++ Varejando/Varejando/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Varejando
+//
+//  Created by Ruan Gustavo de Oliveira on 08/02/20.
+//  Copyright © 2020 Ruan Gustavo de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
